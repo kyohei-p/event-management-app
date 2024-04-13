a83481211b5c99278e55b982f5ead5618de2a44c
kyohei-p <ysdkyh11@gmail.com>
2024-04-13 03:37:40 +0000

    ログイン機能の作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to(root_path)
+    else
+      render action: 'new'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def destroy
+    remember_me!
+    forget_me!
+    logout
+    redirect_to(login_path)
+  end
+end
