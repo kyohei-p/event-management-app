ddcf3ff3e0b272f7341f0da50593dcc240ff47bc
kyohei-p <ysdkyh11@gmail.com>
2024-04-13 08:04:58 +0000

    ログインページのCSSとバリデーション実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,12 @@ class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
   def create
-    @user = login(params[:email], params[:password])
+    @user = login(params[:email], params[:password], params[:remember_me])
 
     if @user
-      redirect_back_or_to(root_path)
+      redirect_back_or_to(root_path, notice: 'ログインに成功しました')
     else
+      flash.now[:alert] = 'ログインに失敗しました'
       render action: 'new'
     end
   end
@@ -19,6 +20,6 @@ class SessionsController < ApplicationController
     remember_me!
     forget_me!
     logout
-    redirect_to(login_path)
+    redirect_to(login_path, notice: 'ログアウトしました')
   end
 end
