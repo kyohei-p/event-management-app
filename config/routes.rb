5b15ee6c4e0384f4c93f3e2be20fab2d8c41339b
kyohei-p <ysdkyh11@gmail.com>
2024-05-11 04:26:06 +0900

    イベント管理ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,14 @@ Rails.application.routes.draw do
 
   get 'events/:event_day/events', to: 'events#index', as: 'date_events'
 
+  # イベント管理ページ
+  scope :manage, as: :manage do
+    get 'events', to: 'events#manage_events'
+
+    resources :categories, as: :category do
+      get 'events', to: 'events#manage_events'
+    end
+
+    get 'events/:event_day/events', to: 'events#manage_events', as: 'date_events'
+  end
 end
